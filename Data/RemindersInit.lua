e240c0e918928550b702f9bdcd74c1bedf670baf
Medalink <medalink7@gmail.com>
2026-03-30 18:15:50 -0500

    data(reminders): refresh dungeon and recommendation content

--- Data/RemindersInit.lua
+++ Data/RemindersInit.lua
@@ -2,7 +2,7 @@ local _, ns = ...
 
 ns.RemindersData = {
     dataVersion   = 1,
-    lastUpdated   = "2026-03-24",
+    lastUpdated   = "2026-03-30",
 
     sources       = {},
     capabilities  = {},
@@ -22,6 +22,7 @@ S.archon = {
     badge = "|cff00ccff[A]|r",
     color = { 0, 0.8, 1.0 },
     url   = "archon.gg",
+    lastFetched = 1774909642,
 }
 
 S.wowhead = {
@@ -29,7 +30,7 @@ S.wowhead = {
     badge = "|cffff8800[W]|r",
     color = { 1.0, 0.53, 0 },
     url   = "wowhead.com",
-    lastFetched = 1774369240,
+    lastFetched = 1774909642,
 }
 
 S.icyveins = {
@@ -37,5 +38,6 @@ S.icyveins = {
     badge = "|cff33cc33[IV]|r",
     color = { 0.2, 0.8, 0.2 },
     url   = "icy-veins.com",
+    lastFetched = 1774909642,
 }
 
